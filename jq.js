2f0f2501b29bb2fb70e0a3f871e5ec84132e18bb
Pastel-Prince <voo0001@jmss.vic.edu.au>
2017-09-11 14:26:16 +1000

    Base Website

---
+++ jq.js
@@ -0,0 +1,33 @@
+var butts = 16
+
+$(document).ready(function(){
+    $(".selectable").click(function(){
+        $(".innerBox").css("background-color", "grey");
+        if ( $(this).is("#B1") || $(this).is("#B2") || $(this).is("#B3") || $(this).is("#B4")) {
+          $(this).css("background-color", "#BA68C8");
+          $("#rightside").css("background-color", "#BA68C8");
+        }
+        if ( $(this).is("#L1") || $(this).is("#L3") || $(this).is("#L5")) {
+          $(this).css("background-color", "#81D4FA");
+          $("#rightside").css("background-color", "#81D4FA");
+        }
+        if ( $(this).is("#T1") || $(this).is("#T2") || $(this).is("#T3") || $(this).is("#T4")) {
+          $(this).css("background-color", "#F06292");
+          $("#rightside").css("background-color", "#F06292");
+        }
+        if ( $(this).is("#C")) {
+          $(this).css("background-color", "#81C784");
+          $("#rightside").css("background-color", "#81C784");
+        }
+    });
+
+    $("#BC").click(function(){
+      $("h2").toggle();
+      $("h3").toggle();
+    });
+
+    $("#Temperature").click(function() {
+      $(this).html(butts +"<sup>o</sup>")
+      butts += 1
+    })
+});
