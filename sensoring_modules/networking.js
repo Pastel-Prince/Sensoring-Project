03ac6e81778532b2c2899ef5d64bc7fdb01488b6
Christopherbikie <christopher.hall.3609@gmail.com>
2017-10-19 18:27:20 +1100

    More verbose messages on sensor connection/disconnection

--- sensoring_modules/networking.js
+++ sensoring_modules/networking.js
@@ -1,6 +1,7 @@
 "use strict";
 var vsprintf = require("sprintf-js").vsprintf;
 var NetworkQueue = require("./networking/network_queue.js").NetworkQueue;
+var sensorManager = require("./sensor_manager.js");
 
 var cs; // clientSocket
 var sr; // sensorReceiver
@@ -20,7 +21,7 @@ function networkingSetup(clientSocket, sensorReceiver, receiveSensorInformation,
         socket.on("disconnect", (reason) => {
             console.log(vsprintf('Client disconnection on socket %s', socket.id));
             delete connectedClients[socket.id];
-        })
+        });
     });
 
     // A sensor has connected on a socket to the server
@@ -28,18 +29,21 @@ function networkingSetup(clientSocket, sensorReceiver, receiveSensorInformation,
         console.log(vsprintf('Sensor connection on socket %s', socket.id));
 
         socket.on("initSensor", (data) => {
-            registerSensorSocket(socket.id, data)
-        })
+            console.log(vsprintf('Sensor for %s connected on socket %s', [data.name, socket.id]));
+            registerSensorSocket(socket.id, data);
+        });
 
-        socket.on("disconnect", (data) => {
-            unregisterSensorSocket(socket.id)
-        })
+        socket.on("disconnect", (reason) => {
+        var room = sensorManager.getSensor(socket.id).name;
+            console.log(vsprintf('Sensor for %s disconnected. Reason: %s', [room, reason]));
+            unregisterSensorSocket(socket.id);
+        });
 
         // Sensor has sent data to the server
         socket.on("receiveSensorInformation", (data) => {
             // Run a callback given to us when setup function was called
             receiveSensorInformation(socket.id, connectedClients, data);
-        })
+        });
     });
 }
 
